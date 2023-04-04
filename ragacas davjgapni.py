54c583693e8bb205395bd43a182ee3e1e52710e0
kvaratskheliagela <113771343+kvaratskheliagela@users.noreply.github.com>
2023-04-04 22:30:20 +0400

    Add files via upload

---
+++ ragacas davjgapni.py
@@ -0,0 +1,17 @@
+#davaleba2
+# def count_paths(n):
+#     if n==1:
+#         return 1
+#     elif n== 2:
+#         return 2
+#     else:
+#         return count_paths(n-1)+count_paths(n-2)
+#dvaleba
+# def int_to_roman(int):
+#     roman_map = {1000: 'M',   100: 'C', 50: 'L', 10: 'X', 5: 'V',  1: 'I'}
+#     roman_num = ''
+#     for value, symbol in roman_map.items():
+#         while num >= value:
+#             roman_num += symbol
+#             num -= value
+#     return roman_num
